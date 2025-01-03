46bd5f9b573972c53c95c2a79359c19ca260ed3d
sophietao127 <sophietao127@gmail.com>
2025-01-03 14:07:50 -0800

    add partner company logo

--- components/partnerCompany.js
+++ components/partnerCompany.js
@@ -1,11 +1,17 @@
 import Logo from "./Logo";
-const logos = [{ src: "/images/people.jpg", alt: "Partner Logo" }];
+const logos = [
+  { src: "/images/partner_company_logo/gptdao_b.png", alt: "GPTDAO Logo" },
+  {
+    src: "/images/partner_company_logo/GenAI_Summit.png",
+    alt: "GenAI Summit Logo",
+  },
+];
 const PartnerCompanyLogos = () => {
   return (
     <section className="py-16 bg-gray-50">
-      <div className="container mx-auto px-4 max-w-6xl">
+      <div className="container mx-auto px-4 max-w-8xl">
         <h2 className="text-3xl font-bold text-center mb-12">
-          The Companies We Are Partnered and Trusted with
+          The Companies We Are Partnered and Trusted By
         </h2>
         <div className="flex flex-wrap gap-6 justify-center">
           {logos.map((item, index) => (
@@ -13,7 +19,7 @@ const PartnerCompanyLogos = () => {
               key={index}
               src={item.src}
               alt={item.alt}
-              width={180}
+              width={200}
               height={60}
             />
           ))}
